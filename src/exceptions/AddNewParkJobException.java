12129ffc8bc8e363eb6d0c9b12525fae03bc1aeb
ChadTC <codecrab.me@gmail.com>
2018-02-09 19:20:34 -0800

    Chad adding in Exceptions we have so far

--- src/exceptions/AddNewParkJobException.java
+++ src/exceptions/AddNewParkJobException.java
@@ -1,16 +1,18 @@
 package exceptions;
 
 /**
- * Created by dave on 2/7/18.
+ * Created by dave on 2/9/18.
  */
-public class InvalidJobEndDateException extends Exception {
+public class AddNewParkJobException extends Exception {
+
     private String msgString = "";
 
-    public InvalidJobEndDateException(final String paramMsgString) {
+    public AddNewParkJobException(final String paramMsgString) {
 
         msgString = paramMsgString;
 
     }
+
     public String getMsgString() {
         return msgString;
     }
