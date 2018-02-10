1639f29f38ffec182884cc04aefb20979612d19e
ChadTC <codecrab.me@gmail.com>
2018-02-09 21:25:01 -0800

    Chad - refactored the Volunteer class, it should function as expected now, Job class is now a mess with redundant logic but that's where the work needs to happen next, then we should be a-ok

--- src/model/Volunteer.java
+++ src/model/Volunteer.java
@@ -2,6 +2,8 @@ package model;
 
 import exceptions.*;
 
+import java.util.HashMap;
+
 public final class Volunteer extends User {
 
 private HashMap<JobID, Job> jobsCurrentlyRegisteredForMap;
@@ -44,8 +46,7 @@ private HashMap<JobID, Job> jobsCurrentlyRegisteredForMap;
         jobsCurrentlyRegisteredForMap = new HashMap<>();
 
     }
-    
-    
+
     /**
 	 * This is the constructor for Volunteer that has jobs signed
 	 * up already.
@@ -57,127 +58,15 @@ private HashMap<JobID, Job> jobsCurrentlyRegisteredForMap;
 	 * @param theJobs the jobs that volunteer signed up already.
 	 */
    public Volunteer(String firstName, String lastName, UserID userID,
-		   				JobMap theJobs) {
+		   				JobCollection theJobs) {
        super(firstName, lastName, UserRole.VOLUNTEER, userID);
        myJobs = theJobs;
        myIsSignedUp = false;
    }
-   
-   /**
-	 * This method would handle the situation when the volunteer is trying 
-	 * to sign up for jobs that extend across the job(s) already signed up for.
-	 * 
-	 * @param theCandidateJob the job that the Volunteer is trying to sign up for.
-	 * @return boolean value shows if the Volunteer can sign up for this job.
-	 */
-	public boolean isJobSameDay(final Job theCandidateJob) 
-			throws JobInSameDayException {
-		for (Job job: myJobs){
-			if (job.isJobOverlapping(theCandidateJob))
-				throw new JobInSameDayException("Sorry, the job you " + 
-						"are applying extends across the job you have" +
-						" signed up already!!");
-		}
-		return true;
-	}
-	
-	/**
-	 * This method checks if the Volunteer is trying to sign up for a
-	 * job that starts at the end date of the jobs that the Volunteer
-	 * has signed up for.
-	 * 
-	 * @param theCandidateJob the job that the Volunteer is trying to sign up for.
-	 * @return boolean value shows if the Volunteer can sign up for this job.
-	 */
-	public boolean isStartAtEndDate(Job theCandidateJob) throws
-						JobStartAtEndDateException {
-		for (Job job: myJobs){
-			if (job.isStartAtEndDate(theCandidateJob))
-				throw new JobStartAtEndDateException("Sory, " + 
-						"the job you are applying starts at " + 
-						"the end date of one of your signed up jobs!");
-		}
-		return true;
-	}
-	
-	/**
-	 * This method checks if the Volunteer is trying to sign up for a
-	 * job that ends at the start date of the jobs that the Volunteer
-	 * has signed up for.
-	 * 
-	 * @param theCandidateJob the job that the Volunteer is trying to sign up for.
-	 * @return boolean value shows if the Volunteer can sign up for this job.
-	 */
-	public boolean isEndAtStartDate(Job theCandidateJob) throws
-					JobEndAtStartDateException {
-		for(Job job: myJobs){
-			if (job.isEndAtStartDate(theCandidateJob))
-				throw new JobEndAtStartDateException("Sorry, " +
-						"the job you are applying ends at the " +
-						"date of one of your signed up jobs!!" );
-		}
-		return true;
-	}
-	
-	/**
-	 * Returns true if the job begins more than or equal to the minimum number of days allowed away from the current 
-	 * date, false otherwise.
-	 * 
-	 * @param candidateJob is the job that the volunteer is attempting to sign up for.
-	 * 
-	 * @return true if the job start date is more than or equal to the minimum number of calendar days out that the 
-	 * volunteer is allowed to sign up for, false otherwise.
-	 */
-	public boolean isMoreThanMinimumDaysOut(Job candidateJob) throws
-						LessThanMinimumDaysAwayException{
-		boolean beyondMin = true;
-		
-		if (candidateJob.dateDifference() < MINIMUM_SIGNUP_DAYS_OUT){
-			beyondMin = false;
-			throw new LessThanMinimumDaysAwayException("Sorry, " +
-					"the job you are applying is less than the " +
-					"minimum days away from one of your signed up jobs!!");
-		}		
-		return beyondMin;
-	}
-	/**
-	public void signupForNewJob(Job theCandidateJob) 
-						throws SignupForNewJobException{
-		if (myIsSignedUp){
-			try{
-				isJobSameDay(theCandidateJob);
-			}
-			catch(JobInSameDayException e){
-				throw new SignupForNewJobException(e.getMsgString());
-			}
-			try{
-				isStartAtEndDate(theCandidateJob);
-			}
-			catch(JobStartAtEndDateException e){
-				throw new SignupForNewJobException(e.getMsgString());
-			} 
-			try{
-				isEndAtStartDate(theCandidateJob);
-			}
-			catch(JobEndAtStartDateException e){
-				throw new SignupForNewJobException(e.getMsgString());
-			}
-			try{
-				isMoreThanMinimumDaysOut(theCandidateJob);
-			}
-			catch(LessThanMinimumDaysAwayException e){
-				throw new SignupForNewJobException(e.getMsgString());
-			}
-		}
-		
-		myJobs.addJob(theCandidateJob);//Should we change this? 
-								//Otherwise it would go through the exceptions 
-								//again.
-	}*/
 
     public void registerForJobInCollection(final Job jobToRegisterFor)
             throws VolunteerJobRegistrationException {
-        //String retStr = "";
+
         try { //let's see if we can sign up for this job
             jobToRegisterFor.addVolunteerToThisJob(this);
         }
@@ -206,5 +95,41 @@ private HashMap<JobID, Job> jobsCurrentlyRegisteredForMap;
     public HashMap<JobID, Job> getJobsCurrentlyRegisteredForMap() {
         return jobsCurrentlyRegisteredForMap;
     }
+
+    /**
+     public void signupForNewJob(Job theCandidateJob)
+     throws SignupForNewJobException{
+     if (myIsSignedUp){
+     try{
+     isJobSameDay(theCandidateJob);
+     }
+     catch(JobInSameDayException e){
+     throw new SignupForNewJobException(e.getMsgString());
+     }
+     try{
+     isStartAtEndDate(theCandidateJob);
+     }
+     catch(JobStartAtEndDateException e){
+     throw new SignupForNewJobException(e.getMsgString());
+     }
+     try{
+     isEndAtStartDate(theCandidateJob);
+     }
+     catch(JobEndAtStartDateException e){
+     throw new SignupForNewJobException(e.getMsgString());
+     }
+     try{
+     isMoreThanMinimumDaysOut(theCandidateJob);
+     }
+     catch(LessThanMinimumDaysAwayException e){
+     throw new SignupForNewJobException(e.getMsgString());
+     }
+     }
+
+     myJobs.addJob(theCandidateJob);//Should we change this?
+     //Otherwise it would go through the exceptions
+     //again.
+     }*/
+
     //end of Volunteer class
 }
