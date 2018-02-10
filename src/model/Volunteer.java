1baa84d26a8dfe0c575bcb450f749d2ac5ce476b
ChadTC <codecrab.me@gmail.com>
2018-02-09 18:21:21 -0800

    Chad refactored Volunteer to use try/catch for adding volunteers to jobs, refactored a few exceptions as well

--- src/model/Volunteer.java
+++ src/model/Volunteer.java
@@ -1,14 +1,44 @@
 package model;
 
+import exceptions.*;
+
 public class Volunteer extends User {
 
-    public Volunteer() {
+    private JobMap volunteerJobMap;
+
+    public Volunteer(JobMap paramJobMap) {
         this("Test", "Volunteer",
                 new UserID("volunteer_default"));
+
+        volunteerJobMap = paramJobMap;
     }
 
     public Volunteer(String firstName, String lastName, UserID userID) {
         super(firstName, lastName, UserRole.VOLUNTEER, userID);
     }
 
+    public void registerForJobInCollection(final Job jobToAdd)
+            throws VolunteerJobRegistrationException {
+        //String retStr = "";
+        try {
+            jobToAdd.addNewVolunteer(this);
+        }
+        catch(VolunteerDailyJobLimitException e ) {
+            throw new VolunteerJobRegistrationException(e.getMsgString());
+        }
+        catch(VolunteerSignUpStartDateException e) {
+            throw new VolunteerJobRegistrationException(e.getMsgString());
+        }
+//        catch (InvalidJobEndDateException e) {
+//            throw new VolunteerJobRegistrationException(e.getMsgString());
+//        }
+        //this should not really ever happen, because if a job already has this
+        //userId associated with it, then it should never display to the Volunteer
+        //object in the first place, right?
+        catch (DuplicateVolunteerUserIDException e) {
+            throw new VolunteerJobRegistrationException(e.getMsgString());
+        }
+
+    }
+    //end of Volunteer class
 }
