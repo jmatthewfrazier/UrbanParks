c53ab6683e0751831701e26550ab862f6bc7d3b8
ChadTC <codecrab.me@gmail.com>
2018-02-09 19:30:48 -0800

    Chad cleaned up Volunteer class, need a way to get the JobJD from the Job Map so the Volunteer can add it to a private collection of Jobs to compare against

--- src/model/Volunteer.java
+++ src/model/Volunteer.java
@@ -6,22 +6,26 @@ public class Volunteer extends User {
 
     private JobMap volunteerJobMap;
 
+    private JobMap jobsCurrentlyRegisteredForMap;
+
     public Volunteer(JobMap paramJobMap) {
         this("Test", "Volunteer",
                 new UserID("volunteer_default"));
 
         volunteerJobMap = paramJobMap;
+        jobsCurrentlyRegisteredForMap = new JobMap();
     }
 
     public Volunteer(String firstName, String lastName, UserID userID) {
         super(firstName, lastName, UserRole.VOLUNTEER, userID);
+        jobsCurrentlyRegisteredForMap = new JobMap();
     }
 
-    public void registerForJobInCollection(final Job jobToAdd)
+    public void registerForJobInCollection(final Job jobToRegisterFor)
             throws VolunteerJobRegistrationException {
         //String retStr = "";
-        try {
-            jobToAdd.addNewVolunteer(this);
+        try { //let's see if we can sign up for this job
+            jobToRegisterFor.addNewVolunteer(this);
         }
         catch(VolunteerDailyJobLimitException e ) {
             throw new VolunteerJobRegistrationException(e.getMsgString());
@@ -29,15 +33,19 @@ public class Volunteer extends User {
         catch(VolunteerSignUpStartDateException e) {
             throw new VolunteerJobRegistrationException(e.getMsgString());
         }
-//        catch (InvalidJobEndDateException e) {
-//            throw new VolunteerJobRegistrationException(e.getMsgString());
-//        }
+
         //this should not really ever happen, because if a job already has this
         //userId associated with it, then it should never display to the Volunteer
         //object in the first place, right?
         catch (DuplicateVolunteerUserIDException e) {
             throw new VolunteerJobRegistrationException(e.getMsgString());
         }
+        //ok, by now all the checks should have been performed
+        //that tells me the Volunteer has successfully registered for the Job
+        //now we need to track this Job for comparisons in the future
+        //so let's get some info from the system's job map:
+
+        JobID newVolunteerJobID = volunteerJobMap.
 
     }
     //end of Volunteer class
