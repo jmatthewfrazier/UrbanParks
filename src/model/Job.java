a4df43b59025a9c2d7253c3e17d16da2a2739f55
ChadTC <codecrab.me@gmail.com>
2018-02-12 11:04:25 -0800

    Chad - commit to pull in changes for fininshed deliv2

--- src/model/Job.java
+++ src/model/Job.java
@@ -1,6 +1,7 @@
 package model;
 
 import exceptions.DuplicateVolunteerUserIDException;
+import exceptions.JobInSameDayException;
 import exceptions.VolunteerDailyJobLimitException;
 import exceptions.VolunteerSignUpStartDateException;
 
@@ -160,6 +161,86 @@ public class Job implements Serializable {
 	public boolean isEndAtStartDate(Job theCandidateJob){
 		return this.myStartDate.equals(theCandidateJob.myEndDate);
 	}
+
+    /**
+     * This method would handle the situation when the volunteer is trying
+     * to sign up for jobs that extend across the job(s) already signed up for.
+     *
+     * @param theCandidateJob the job that the Volunteer is trying to sign up for.
+     * @return boolean value shows if the Volunteer can sign up for this job.
+     */
+    public boolean isJobSameDay(final Job theCandidateJob)
+            throws JobInSameDayException {
+        for (Job job: myJobs){
+            if (job.isJobOverlapping(theCandidateJob))
+                throw new JobInSameDayException("Sorry, the job you " +
+                        "are applying extends across the job you have" +
+                        " signed up already!!");
+        }
+        return true;
+    }
+
+    /**
+     * This method checks if the Volunteer is trying to sign up for a
+     * job that starts at the end date of the jobs that the Volunteer
+     * has signed up for.
+     *
+     * @param theCandidateJob the job that the Volunteer is trying to sign up for.
+     * @return boolean value shows if the Volunteer can sign up for this job.
+     */
+    public boolean isStartAtEndDate(Job theCandidateJob) throws
+            JobStartAtEndDateException {
+        for (Job job: myJobs){
+            if (job.isStartAtEndDate(theCandidateJob))
+                throw new JobStartAtEndDateException("Sory, " +
+                        "the job you are applying starts at " +
+                        "the end date of one of your signed up jobs!");
+        }
+        return true;
+    }
+
+    /**
+     * This method checks if the Volunteer is trying to sign up for a
+     * job that ends at the start date of the jobs that the Volunteer
+     * has signed up for.
+     *
+     * @param theCandidateJob the job that the Volunteer is trying to sign up for.
+     * @return boolean value shows if the Volunteer can sign up for this job.
+     */
+    public boolean isEndAtStartDate(Job theCandidateJob) throws
+            JobEndAtStartDateException {
+        for(Job job: myJobs){
+            if (job.isEndAtStartDate(theCandidateJob))
+                throw new JobEndAtStartDateException("Sorry, " +
+                        "the job you are applying ends at the " +
+                        "date of one of your signed up jobs!!" );
+        }
+        return true;
+    }
+
+    /**
+     * Returns true if the job begins more than or equal to the minimum number of days allowed away from the current
+     * date, false otherwise.
+     *
+     * @param candidateJob is the job that the volunteer is attempting to sign up for.
+     *
+     * @return true if the job start date is more than or equal to the minimum number of calendar days out that the
+     * volunteer is allowed to sign up for, false otherwise.
+     */
+    public boolean isMoreThanMinimumDaysOut(Job candidateJob) throws
+            LessThanMinimumDaysAwayException{
+        boolean beyondMin = true;
+
+        if (candidateJob.dateDifference() < MINIMUM_SIGNUP_DAYS_OUT){
+            beyondMin = false;
+            throw new LessThanMinimumDaysAwayException("Sorry, " +
+                    "the job you are applying is less than the " +
+                    "minimum days away from one of your signed up jobs!!");
+        }
+        return beyondMin;
+    }
+
+
 
     public LocalDateTime getBeginDateTime() {
         return beginDateTime;
