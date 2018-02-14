f568fac598708c50c0ad9374bed542e8dbab3b34
ChadTC <codecrab.me@gmail.com>
2018-02-14 00:26:27 -0800

    Chad - got started on the GUI, made GUI branch

---
+++ src/UrbanParksMain.java
@@ -0,0 +1,17 @@
+import view.UrbanParksGUI;
+
+/**
+ * Created by dave on 2/13/18.
+ */
+public class UrbanParksMain {
+
+    private UrbanParksMain() {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        new UrbanParksGUI().display();
+    }
+}
---
+++ src/tests/GUISandbox.java
@@ -0,0 +1,16 @@
+package tests;
+
+import view.UrbanParksGUI;
+
+/**
+ * Created by dave on 2/13/18.
+ */
+public class GUISandbox {
+
+    public static void main(String[] args) {
+
+        UrbanParksGUI upgui = new UrbanParksGUI();
+        upgui.run();
+
+    }
+}
