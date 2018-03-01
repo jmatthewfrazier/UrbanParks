e2742bdda3584d92ef78057ea5e0bf5d2f323d25
Eli Shafer <eli.shafer96@gmail.com>
2018-03-01 13:12:51 -0800

    Park cleaned.

--- src/model/Park.java
+++ src/model/Park.java
@@ -6,44 +6,39 @@ import java.util.List;
 
 public final class Park implements Serializable {
 
-    private String name;
-
+    private final String name;
     private int houseNumber;
-
-    private String street;
-
-    private String city;
-
-    private String state;
-
+    private final String street;
+    private final String city;
+    private final String state;
     private int zipCode;
-
-    private ParkID ID;
-
-    private List<Job> jobs;
+    private final ParkID ID;
+    private final List<Job> jobs;
 
     public Park() {
         this("Stanley Park", 1000, "Apple St.",
                 "Seattle", "WA", 98105, new ParkID(1));
     }
 
-    public Park(String name, int houseNumber, String street, String city,
-                String state, int zipCode, ParkID parkID) {
+    public Park(final String name, int houseNumber, final String street,
+                final String city, final String state, int zipCode,
+                final ParkID parkID) {
         this.name = name;
         this.houseNumber = houseNumber;
         this.street = street;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
+        this.ID = parkID;
         this.jobs = new ArrayList<>();
-        ID = parkID;
     }
 
-    public ParkID getID() {
-        return ID;
+    public final ParkID getID() {
+        return this.ID;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public final String toString() {
+        return this.name;
     }
 }
