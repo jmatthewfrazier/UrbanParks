42885279ac893ffe03d31ddb840b157a1748c714
jmatthewfrazier <jmf87@uw.edu>
2018-03-01 14:54:11 -0800

    merging with master

---
+++ src/tests/TestSuite.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  JobCollectionTest.class,
+  JobTest.class,
+  VolunteerTest.class
+})
+
+public class TestSuite {
+
+}
