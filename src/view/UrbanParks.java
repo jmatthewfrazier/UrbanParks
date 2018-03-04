fb7a59a2b454275533c6fdf58db502c33488db5e
jmatthewfrazier <jmf87@uw.edu>
2018-03-04 15:08:44 -0800

    merging with master

--- src/view/UrbanParks.java
+++ src/view/UrbanParks.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
